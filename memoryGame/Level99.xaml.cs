ae7f4f0f5ae565593fc6c4fbf67df30c32125d30
Saad Khan <saaadkhan23@yahoo.ca>
2017-10-10 13:36:07 -0400

    level 3 fixed

--- memoryGame/Level99.xaml.cs
+++ memoryGame/Level99.xaml.cs
@@ -29,11 +29,197 @@ namespace memoryGame
 
         }
 
+<<<<<<< HEAD
+        private void six_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void five_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void four_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void three_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void seven_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twelve_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eleven_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ten_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nine_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nine_Copy2_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eight_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirteen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void seventeen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sixteen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fifteen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourteen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nineteen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentyfour_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentythree_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentyone_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twenty_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirty_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentynine_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentyseven_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirtysix_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirtyfive_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirtyfour_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirtythree_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirtytwo_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentytwo_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void one_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eighteen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentyfive_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentyfive_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentyeight_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twentysix_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirtyone_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+=======
         private void backBtn_Click(object sender, RoutedEventArgs e)
         {
             var win = new MainWindow();
             win.Show();
             this.Close();
+>>>>>>> 5d30c954928af3f40cdde366b943db1cef559528
         }
     }
 }
